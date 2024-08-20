beef8c09a445a034285ad68cfc1c4ad44823505f
ArtPleb2868 <plebanczyk.artur@gmail.com>
2024-08-20 12:16:19 +0200

    Zmiana nazwy zdjęcia

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <header>
+      <h1 className='flex-center'>PARAFIA PRZYBYSŁAWICE</h1>
+    </header>
   )
 }
 
