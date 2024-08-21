e0e2419974f2e9bf8aa38fa9afafb85d9f649c1f
JcobV01 <jakubwadycki@gmail.com>
2024-08-21 13:21:47 +0200

    kalendarz liturgiczny marginesy -4

--- components/home/kalendarz/Kalendarz.jsx
+++ components/home/kalendarz/Kalendarz.jsx
@@ -11,7 +11,7 @@ const Kalendarz = () => {
     <section id="kalendarz" className='my-[140px] flex flex-col gap-[70px] sm:my-[100px] sm:gap-0'>
       <Title title="Kalendarz" title2="Liturgiczny" subtitle="Czytania na każdy dzień"/>
 
-      <article className='flex justify-between w-[1075px] gap-[25px] m-auto mt-[50px] xl:w-[800px] lg:w-[100%] lg:px-[20px] xl:flex-col'>
+      <article className='flex justify-between w-[1075px] gap-[25px] m-auto xl:w-[800px] lg:w-[100%] lg:px-[20px] xl:flex-col'>
         <div className='flex gap-[20px] bg-[#5A7889] rounded-[5px] items-center w-[275px] p-[15px] xl:w-full xl:flex-1'>
           <Image src={rlIcon} width={50} height={50} alt="Ikona do sekcji rok liturgiczny"/>
           <p className='text-white text-[17px]'>Rok liturgiczny: <span className='font-semibold'>B</span></p>
