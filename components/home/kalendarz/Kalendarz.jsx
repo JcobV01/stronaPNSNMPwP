8f0b2a85168016de9d56078693876f3622b12cf7
JcobV01 <jakubwadycki@gmail.com>
2024-08-21 13:10:14 +0200

    kalendarz liturgiczny marginesy -2

--- components/home/kalendarz/Kalendarz.jsx
+++ components/home/kalendarz/Kalendarz.jsx
@@ -8,7 +8,7 @@ import DayCard from './components/DayCard'
 
 const Kalendarz = () => {
   return (
-    <section id="kalendarz" className='my-[140px] flex flex-col gap-[70px] sm:my-[100px] sm:gap-[30px]'>
+    <section id="kalendarz" className='my-[140px] flex flex-col gap-[70px] sm:my-[100px] sm:gap-0'>
       <Title title="Kalendarz" title2="Liturgiczny" subtitle="Czytania na każdy dzień"/>
 
       <article className='flex justify-between w-[1075px] gap-[25px] m-auto mt-[50px] xl:w-[800px] lg:w-[100%] lg:px-[20px] xl:flex-col'>
