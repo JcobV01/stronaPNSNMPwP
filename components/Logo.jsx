866c812b3cd11b1b8d778d69c947a46001ef207d
JcobV01 <jakubwadycki@gmail.com>
2024-08-21 09:17:50 +0200

    link w logo

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -2,17 +2,20 @@ import Image from 'next/image'
 import React from 'react'
 
 import logo from '@public/assets/icons/logo.svg'
+import Link from 'next/link'
 
 const Logo = ({width, height, padding, backgroundOpacity}) => {
   return (
-    <div className='bg-white rounded-[15px]' style={{padding: padding ? 10 : 20, background: backgroundOpacity ? 'rgba(255,255,255,0.80)' : 'rgba(255,255,255,0.60)'}}>
-        <Image 
-            src={logo}
-            width={width}
-            height={height}
-            alt='Logo parafii'
-        />
-    </div>
+    <Link href="/#homepage">
+      <div className='bg-white rounded-[15px]' style={{padding: padding ? 10 : 20, background: backgroundOpacity ? 'rgba(255,255,255,0.80)' : 'rgba(255,255,255,0.60)'}}>
+          <Image 
+              src={logo}
+              width={width}
+              height={height}
+              alt='Logo parafii'
+          />
+      </div>
+    </Link>
   )
 }
 
