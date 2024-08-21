b13af706abf13106abbc272b50d8b39a7c7ca62b
ArtPleb2868 <plebanczyk.artur@gmail.com>
2024-08-21 13:03:36 +0200

    Media do sekcji parafia

--- components/home/parafia/Parafia.jsx
+++ components/home/parafia/Parafia.jsx
@@ -11,31 +11,31 @@ import sacramentsCard from '@public/assets/images/parish-cards/sacraments-card.w
 const Parafia = () => {
   return (
     <section id="parafia">
-      <div className='pt-[70px] flex-center flex-col'>
+      <div className='pt-[70px] flex-center flex-col sm:pt-[40px]'>
         <Title title="Parafia" title2="" subtitle="Informacje ogólne" />
-        <article className='grid grid-cols-4 gap-[25px] mt-[100px]'>
+        <article className='grid grid-cols-4 gap-[25px] mt-[70px] xl:grid-cols-2 sm:grid-cols-1 sm:mt-[40px] sm:w-full'>
 
-          <div className="col-span-2 row-span-1 w-[525px] h-[250px] rounded-[15px]" style={{ backgroundImage: `url(${historyCard.src})` }}>
+          <div className="col-span-2 row-span-1 w-[525px] h-[250px] rounded-[15px] xl:col-span-2 sm:mx-auto sm:w-[250px] sm:col-span-1" style={{ backgroundImage: `url(${historyCard.src})` }}>
             <TitleCard subtitle={'DZIEJE PARAFII'} title={'Historia'} link={'/historia'} />
           </div>
 
-          <div className="col-start-3 row-span-2 w-[250px] h-[400px] rounded-[15px]" style={{ backgroundImage: `url(${eventCard.src})` }}>
+          <div className="col-start-3 row-span-2 w-[250px] h-[400px] rounded-[15px] xl:col-span-1 sm:mx-auto sm:h-[250px]" style={{ backgroundImage: `url(${eventCard.src})` }}>
             <TitleCard subtitle={'GODZINY NABOŻEŃSTW'} title={'Nabożeństwa'} link={'/nabozenstwa'} />
           </div>
 
-          <div className="col-start-4 row-span-2 w-[250px] h-[400px] rounded-[15px]" style={{ backgroundImage: `url(${nationalsCard.src})` }}>
+          <div className="col-start-4 row-span-2 w-[250px] h-[400px] rounded-[15px] xl:col-span-1 sm:mx-auto sm:h-[250px]" style={{ backgroundImage: `url(${nationalsCard.src})` }}>
             <TitleCard subtitle={'KSIĘŻA I SIOSTRY ZAKONNE'} title={'Rodacy'} link={'/rodacy'} />
           </div>
 
-          <div className="row-span-3 w-[250px] h-[400px] rounded-[15px]" style={{ backgroundImage: `url(${priestsCard.src})` }}>
+          <div className="row-span-3 w-[250px] h-[400px] rounded-[15px] xl:col-span-1 sm:mx-auto sm:h-[250px]" style={{ backgroundImage: `url(${priestsCard.src})` }}>
             <TitleCard subtitle={'KSIĘŻA W NASZEJ PARAFII'} title={'Duszpasterze'} link={'/duszpasterze'} />
           </div>
 
-          <div className="row-span-3 w-[250px] h-[400px] rounded-[15px]" style={{ backgroundImage: `url(${cemeteryCard.src})` }}>
+          <div className="row-span-3 w-[250px] h-[400px] rounded-[15px] xl:col-span-1 sm:mx-auto sm:h-[250px]" style={{ backgroundImage: `url(${cemeteryCard.src})` }}>
             <TitleCard subtitle={'REGULAMIN'} title={'Cmentarz'} link={'/cmentarz'} />
           </div>
 
-          <div className="col-start-3 col-span-2 row-start-3 w-[525px] h-[250px] rounded-[15px]" style={{ backgroundImage: `url(${sacramentsCard.src})` }}>
+          <div className="col-start-3 col-span-2 row-start-3 w-[525px] h-[250px] rounded-[15px] xl:col-span-2 xl:row-start-7 sm:col-span-1 sm:mx-auto sm:w-[250px]" style={{ backgroundImage: `url(${sacramentsCard.src})` }}>
             <TitleCard subtitle={'INFORMACJE'} title={'Sakramenty'} link={'/sakramenty'} />
           </div>
         </article>
