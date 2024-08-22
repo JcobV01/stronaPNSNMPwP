d9b0953ba03c5c9ffca48aa569ba1df7c6b59e86
ArtPleb2868 <plebanczyk.artur@gmail.com>
2024-08-22 14:41:26 +0200

    Bierzmowanie 1/2

--- components/home/sakramenty/Confirmation.jsx
+++ components/home/sakramenty/Confirmation.jsx
@@ -2,7 +2,44 @@ import React from 'react'
 
 const Confirmation = () => {
   return (
-    <div>Confirmation</div>
+    <article className='w-[1400px]'>
+      <h4 className='text-[40px] font-medium tracking-[8px] text-center'>Bierzmowanie</h4>
+      <div className='mt-[15px]'>
+        <h5 className='text-[25px] tracking-[10px] text-center'>Czym jest sakrament bierzmowania?</h5>
+        <p className='text-[18px] tracking-[1.8px] mt-[30px]'>Sakrament bierzmowania jest dopełnieniem sakramentu chrztu św. Bierzmowanie wraz z chrztem i Eucharystią należy do “sakramentów wtajemniczenia chrześcijańskiego”, którego jedność powinna być zachowywana (Katechizm Kościoła Katolickiego 1285).</p>
+      </div>
+      <div className='text-center mt-[50px]'>
+        <h5 className='text-[25px] tracking-[10px]'>Co jest znakiem widzialnym sakramentu bierzmowania?</h5>
+        <p className='text-[18px] tracking-[1.8px] mt-[30px]'>Jest włożenie rąk biskupa, namaszczenie krzyżmem św. i modlitwa.</p>
+      </div>
+      <div className='mt-[50px]'>
+        <h5 className='text-[25px] tracking-[10px] text-center'>Co sprawia Duch św. w sakramencie bierzmowania?</h5>
+        <ul className='mt-[30px] tracking-[1.8px] text-[18px]'>
+          <li>– zakorzenia nas głębiej w Bożym synostwie, tak że możemy mówić “Abba, Ojcze!”,</li>
+          <li>– ściślej jednoczy nas z Chrystusem,</li>
+          <li>– pomnaża w nas Swoje dary,</li>
+          <li>– udoskonala naszą więź z Kościołem,</li>
+          <li>– udziela nam, jako prawdziwym świadkom Chrystusa, Swojej mocy do szerzenia i obrony wiary słowem i czynem, do mężnego wyznawania imienia Chrystusa oraz do tego, by nigdy nie wstydzić się Krzyża (Katechizm Kościoła Katolickiego 1303).</li>
+        </ul>
+      </div>
+      <div className='text-center mt-[50px]'>
+        <h5 className='text-[25px] tracking-[10px]'>Co znaczy apostołować?</h5>
+        <p className='text-[18px] tracking-[1.8px] mt-[30px]'>To troszczyć się nie tylko o swoje zbawienie, ale i zbawienie drugich.</p>
+      </div>
+      <div className='text-center mt-[50px]'>
+        <h5 className='text-[25px] tracking-[10px]'>W jaki sposób chrześcijanin może apostołować?</h5>
+        <p className='text-[18px] tracking-[1.8px] mt-[30px]'>To troszczyć się nie tylko o swoje zbawienie, ale i zbawienie drugich.</p>
+      </div>
+      <div className='text-center mt-[50px]'>
+        <h5 className='text-[25px] tracking-[10px]'>Gdzie chrześcijanin powinien apostołować?</h5>
+        <p className='text-[18px] tracking-[1.8px] mt-[30px]'>Wszędzie, gdzie spotyka się z ludźmi.</p>
+      </div>
+      <div className='mt-[50px]'>
+        <h5 className='text-[25px] tracking-[10px] text-center'>W naszej parafii</h5>
+        <p className='text-[18px] tracking-[1.8px] mt-[30px]'>Przygotowanie do sakramentu bierzmowania dokonuje się w ciągu trzech lat szkoły podstawowej. Polega na formacji duchowo-intelektualnej kandydata do bierzmowania. Spotkania w naszej parafii odbywają się co miesiąc po wcześniejszym ustaleniu terminu.</p>
+        <p className='text-[18px] tracking-[1.8px] mt-[10px]'>Warto przeczytać: Katechizm Kościoła Katolickiego dla Młodych/Youcat/ nr 203 i następne oraz KKK nr 1285-1314.</p>
+      </div>
+    </article>
   )
 }
 
