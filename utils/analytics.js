0ffa1a50fb6c277cd89fc4aa4738d35cc6d0a88e
ArtPleb2868 <plebanczyk.artur@gmail.com>
2024-08-28 12:48:08 +0200

    XD

--- utils/analytics.js
+++ utils/analytics.js
@@ -1,39 +1,22 @@
 import { connectMongoDB } from "@utils/database";
 import Event from "@models/events";
 
-// export class Analytics {
-//     constructor(opts = {}) {
-//         // Domyślna wartość retention to 60 sekund * 60 minut * 24 godziny * 7 dni
-//         this.retention = 60 * 60 * 24 * 7;
+export class Analytics {
+    constructor(opts = {}) {
+        // Domyślna wartość retention to 60 sekund * 60 minut * 24 godziny * 7 dni
+        this.retention = 60 * 60 * 24 * 7;
 
-//         // Jeśli opts.retention jest podane, nadpisujemy domyślną wartość
-//         if (opts.retention) {
-//             this.retention = opts.retention;
-//         }
-//     }
+        // Jeśli opts.retention jest podane, nadpisujemy domyślną wartość
+        if (opts.retention) {
+            this.retention = opts.retention;
+        }
+    }
 
-//     async track(namespace) {
-//         const key = `analytics::${namespace}`
-        
-//         // Połączenie z MongoDB przed rozpoczęciem śledzenia
-//         await connectMongoDB();
-
-//         // Używamy operacji upsert do dodania lub aktualizacji licznika
-//         // await Event.updateOne(
-//         //     { _id: key },
-//         //     { $inc: { count: 1 }, $set: { lastEvent: event, updatedAt: new Date() } },
-//         //     { upsert: true }
-//         // );
-//     }
-// }
-
-
-
-export const track = async (namespace, event) => {
-    const key = `analytics::${namespace}`
-    await connectMongoDB();
+    async track(namespace) {
+        const key = `analytics::${namespace}`
         
         // Połączenie z MongoDB przed rozpoczęciem śledzenia
+        await connectMongoDB();
 
         // Używamy operacji upsert do dodania lub aktualizacji licznika
         await Event.updateOne(
@@ -42,6 +25,23 @@ export const track = async (namespace, event) => {
             { upsert: true }
         );
     }
+}
+
+
+
+// export const track = async (namespace, event) => {
+//     const key = `analytics::${namespace}`
+//     await connectMongoDB();
+        
+//         // Połączenie z MongoDB przed rozpoczęciem śledzenia
+
+//         // Używamy operacji upsert do dodania lub aktualizacji licznika
+//         await Event.updateOne(
+//             { _id: key },
+//             { $inc: { count: 1 }, $set: { lastEvent: event, updatedAt: new Date() } },
+//             { upsert: true }
+//         );
+//     }
 
     // export const analytics = async () => {
     //     await connectMongoDB();
