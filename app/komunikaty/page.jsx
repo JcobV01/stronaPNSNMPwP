3810e8d7260b13c0cb65b830d07364dbac58d8ea
ArtPleb2868 <plebanczyk.artur@gmail.com>
2024-10-26 12:07:07 +0200

    Filtry do inputa search, dodanie wyłączenia paginacji przy liczbie postów mniejszej niż 10

--- app/komunikaty/page.jsx
+++ app/komunikaty/page.jsx
@@ -7,6 +7,7 @@ import { Icon } from '@iconify/react'
 
 const komunikaty = () => {
   const [posts, setPosts] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPosts, setTotalPosts] = useState(0);
 
@@ -30,8 +31,6 @@ const komunikaty = () => {
     fetchPosts();
   }, [currentPage]);
 
-  const totalPages = Math.ceil(totalPosts / postsPerPage);
-
   const handlePageChange = (page) => {
     setCurrentPage(page);
   };
@@ -48,27 +47,41 @@ const komunikaty = () => {
     }
   };
 
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const filteredPosts = posts.filter(post =>
+    post.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  const totalPages = searchTerm
+    ? Math.ceil(filteredPosts.length / postsPerPage)
+    : Math.ceil(totalPosts / postsPerPage);
+
   return (
     <section className='flex-center flex-col pb-[250px]'>
       <div className='relative top-[-30px]'>
-        <input type="search" placeholder="Wyszukaj po tytule" className='w-[815px] h-[60px] rounded-[30px] pl-[55px] pr-[25px] outline-none' />
+        <input type="search" value={searchTerm} onChange={handleSearchChange} placeholder="Wyszukaj po tytule" className='w-[815px] h-[60px] rounded-[30px] pl-[55px] pr-[25px] outline-none' />
         <Icon icon="stash:search" width="40px" height="40px" className='text-[#5A7889] absolute top-[10px] right-[25px]' />
       </div>
       <Title title="Komunikaty" title2="" subtitle="Najnowsze posty" />
       <article className='mt-[50px]'>
-        {posts?.map((post) => (
+        {filteredPosts?.map((post) => (
           <Posts key={post._id} post={post} />
         ))}
       </article>
-      <div className='flex items-center justify-end mt-[20px] gap-2'>
-        <button onClick={() => handlePageChange(1)} disabled={currentPage === 1} className='text-[24.5px] font-light'>&laquo;</button>
-        <button onClick={handlePrevPage} disabled={currentPage === 1} className='text-[20px] font-light'>&lt;</button>
-        {Array.from({ length: totalPages }, (_, index) => index + 1).map(page => (
-          <button key={page} onClick={() => handlePageChange(page)} className={currentPage === page ? 'font-medium text-[25px]' : 'font-light text-[20px]'}>{page}</button>
-        ))}
-        <button onClick={handleNextPage} disabled={currentPage === totalPages} className='text-[20px] font-light'>&gt;</button>
-        <button onClick={() => handlePageChange(totalPages)} disabled={currentPage === totalPages} className='text-[24.5px] font-light'>&raquo;</button>
-      </div>
+      {totalPages > 1 && (
+        <div className='flex items-center justify-end mt-[20px] gap-2'>
+          <button onClick={() => handlePageChange(1)} disabled={currentPage === 1} className='text-[24.5px] font-light'>&laquo;</button>
+          <button onClick={handlePrevPage} disabled={currentPage === 1} className='text-[20px] font-light'>&lt;</button>
+          {Array.from({ length: totalPages }, (_, index) => index + 1).map(page => (
+            <button key={page} onClick={() => handlePageChange(page)} className={currentPage === page ? 'font-medium text-[25px]' : 'font-light text-[20px]'}>{page}</button>
+          ))}
+          <button onClick={handleNextPage} disabled={currentPage === totalPages} className='text-[20px] font-light'>&gt;</button>
+          <button onClick={() => handlePageChange(totalPages)} disabled={currentPage === totalPages} className='text-[24.5px] font-light'>&raquo;</button>
+        </div>
+      )}
     </section>
   )
 }
