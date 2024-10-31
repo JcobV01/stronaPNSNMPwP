a4022a452f41484e3fa5d4f5bc429c335e42bea0
JcobV01 <jakubwadycki@gmail.com>
2024-10-31 12:33:56 +0100

    zmiana połączenia z bazą

--- utils/database.js
+++ utils/database.js
@@ -3,13 +3,15 @@ import mongoose from 'mongoose';
 let isConnected = false;
 
 export const connectMongoDB = async () => {
+  mongoose.set('strictQuery', true);
+  
   if (isConnected) {
     console.log('MongoDB is already connected');
     return;
   }
 
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI, {dbName: "parafia_przybysl"});
     isConnected = true;
     console.log("Connected to MongoDB");
   } catch (error) {
