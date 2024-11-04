cba0854131f7f863d26e4a870a4dc5ca5e907c93
ArtPleb2868 <plebanczyk.artur@gmail.com>
2024-11-04 12:14:51 +0100

    Animacje do rodaków

--- app/rodacy/page.jsx
+++ app/rodacy/page.jsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Title from '@components/Title'
 
 import ksKozyra from '@public/assets/images/priests/ks-kazimierz.webp'
@@ -6,6 +8,7 @@ import ksIcon from '@public/assets/icons/nationals/priest.svg'
 import sIcon from '@public/assets/icons/nationals/sister.svg'
 import NPriestsCard from '@components/nationals/NPriestsCard'
 import NSistersCard from '@components/nationals/NSistersCard'
+import useIntersectionObserver from '@hooks/useObserver'
 
 const rodacy = () => {
 
@@ -67,23 +70,31 @@ const rodacy = () => {
     },
   ]
 
+  const [refOne, isVisibleOne] = useIntersectionObserver({
+    threshold: 0.1 // 10% widoczności sekcji wystarczy do uruchomienia animacji
+  });
+
+  const [refTwo, isVisibleTwo] = useIntersectionObserver({
+    threshold: 0.1 // 10% widoczności sekcji wystarczy do uruchomienia animacji
+  });
+
   return (
     <section className='pt-[50px] flex flex-col items-center px-[20px] pb-[100px]'>
       <Title title="Rodacy" title2="" subtitle="Księża i siostry zakonne"/>
 
       <h4 className='text-[25px] font-semibold tracking-[2.5px] text-center mt-[70px] mb-[60px] lg:text-[20px]'>Księża</h4>
 
-      <div className='flex flex-wrap w-[1250px] justify-center gap-[30px] xl:w-full'>
-        {priests.map((priest) => (
-          <NPriestsCard image={priest.image} name={priest.name.toUpperCase()} desc={priest.desc} alive={priest.alive} dimensions={priest.dimensions}/>
+      <div ref={refOne} className={`flex flex-wrap w-[1250px] justify-center gap-[30px] transition-all duration-1000 ease-in-out ${isVisibleOne ? 'animation-visible' : 'animation-hidden'} xl:w-full`}>
+        {priests.map((priest, index) => (
+          <NPriestsCard key={index} image={priest.image} name={priest.name.toUpperCase()} desc={priest.desc} alive={priest.alive} dimensions={priest.dimensions}/>
         ))}
       </div>
 
       <h4 className='text-[25px] font-semibold tracking-[2.5px] text-center mt-[70px] mb-[60px] lg:text-[20px]'>Siostry zakonne</h4>
 
-      <div className='flex flex-wrap w-[1250px] justify-center gap-[30px] xl:w-full'>
-        {sisters.map((sister) => (
-          <NSistersCard image={sister.image} name={sister.name.toUpperCase()} desc={sister.desc} alive={sister.alive}/>
+      <div ref={refTwo} className={`flex flex-wrap w-[1250px] justify-center gap-[30px] transition-all duration-1000 ease-in-out ${isVisibleTwo ? 'animation-visible' : 'animation-hidden'} xl:w-full`}>
+        {sisters.map((sister, index) => (
+          <NSistersCard key={index} image={sister.image} name={sister.name.toUpperCase()} desc={sister.desc} alive={sister.alive}/>
         ))}
       </div>
 
