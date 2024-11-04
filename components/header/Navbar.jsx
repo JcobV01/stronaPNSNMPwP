f6be5efb5760becd8f02996bc24e2c8a0cded70e
JcobV01 <jakubwadycki@gmail.com>
2024-11-04 10:02:35 +0100

    menu hamburger - 2

--- components/header/Navbar.jsx
+++ components/header/Navbar.jsx
@@ -40,7 +40,7 @@ const Navbar = () => {
           <div className={active ? "active-hamburger absolute top-[50%] translate-y-[-50%]" : "custom-hamburger absolute top-[50%] translate-y-[-50%]"}></div>
         </div>
 
-        <div className={active ? "hidden md:flex justify-center gap-4 absolute top-0 left-0 w-full h-screen bg-[rgba(0,0,0,0.90)] z-[110] transition-all duration-500 ease-in-out transform translate-x-0" : "fixed md:hidden w-full h-screen bg-white transition-all duration-500 ease-in-out transform translate-x-full"}>
+        <div className={active ? "hidden md:flex justify-center gap-4 absolute top-0 left-0 w-full h-dvh bg-[rgba(0,0,0,0.90)] z-[110] transition-all duration-500 ease-in-out transform translate-x-0" : "fixed md:hidden w-full h-screen bg-white transition-all duration-500 ease-in-out transform translate-x-full"}>
           <nav className='flex-center flex-col h-full text-white gap-5'>
             <Link href="/#parafia">Parafia</Link>
             <Link href="/#grupy">Grupy parafialne</Link>
