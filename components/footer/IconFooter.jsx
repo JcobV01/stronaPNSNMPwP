ad97258d83d50795af622a421ba946f2ca58b9dc
ArtPleb2868 <plebanczyk.artur@gmail.com>
2024-11-04 10:06:24 +0100

    Naprawa błędów w footer z ikonami

--- components/footer/IconFooter.jsx
+++ components/footer/IconFooter.jsx
@@ -6,7 +6,7 @@ const IconFooter = ({imgSource, destination, desc}) => {
   return (
     <div className='bg-none border-[2.5px] border-white rounded-full p-[5px]'>
         <Link href={destination} target='_blank' title={`Przejdź do ${desc}`} aria-label={desc} >
-            <Icon icon={imgSource} width="35" height="35" alt='Ikona socialmedia' className='text-[#fff]'/>
+            <Icon icon={imgSource} width="35" height="35" alt={`Ikona ${desc}`} className='text-[#fff]'/>
         </Link>
     </div>
   )
