0b8cae68e19d588813371ee4ec396c95c241a59e
JcobV01 <jakubwadycki@gmail.com>
2024-11-04 08:05:39 +0100

    suspense - wiadomosci/szczegoly

--- app/management/panel/wiadomosci/szczegoly/page.jsx
+++ app/management/panel/wiadomosci/szczegoly/page.jsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useSearchParams } from 'next/navigation';
-import React, { useEffect, useState } from 'react'
+import React, { Suspense, useEffect, useState } from 'react'
 
 const page = () => {
   const [message, setMessage] = useState({
@@ -44,22 +44,24 @@ const page = () => {
   }, [messageId])
 
   return (
-    <section className='mt-[10px] w-full'>
-      <article>
-        <div className='flex items-center justify-between'>
-          <h2 className='text-[25px] font-light'>Wiadomość od <span className='font-semibold text-[#5A7889]'>{message.name}</span></h2>
-          <p className='text-[18px] font-semibold'>Wysłano {message.date}</p>
-        </div>
-        <div className='flex items-center justify-between'>
-          <p className='text-[18px] font-light mt-[15px]'>Z emailu <span className='font-semibold text-[#5A7889]'>{message.email}</span></p>
-          <p className='text-[18px] font-semibold'>O godzinie {message.time}</p>
-        </div>
-        <p className='text-[18px] font-light'>Do <span className='font-semibold text-[#5A7889]'>{message.objective}</span></p>
-        <hr className='w-full mt-[15px] border-[#5A7889]' />
-        <p className='mt-[15px] text-[18px] font-light'>Treść</p>
-        <p className='mt-[10px] h-[625px] overflow-y-auto pr-[20px]'>{message.message}</p>
-      </article>
-    </section>
+    <Suspense fallback="Ładowanie...">
+      <section className='mt-[10px] w-full'>
+        <article>
+          <div className='flex items-center justify-between'>
+            <h2 className='text-[25px] font-light'>Wiadomość od <span className='font-semibold text-[#5A7889]'>{message.name}</span></h2>
+            <p className='text-[18px] font-semibold'>Wysłano {message.date}</p>
+          </div>
+          <div className='flex items-center justify-between'>
+            <p className='text-[18px] font-light mt-[15px]'>Z emailu <span className='font-semibold text-[#5A7889]'>{message.email}</span></p>
+            <p className='text-[18px] font-semibold'>O godzinie {message.time}</p>
+          </div>
+          <p className='text-[18px] font-light'>Do <span className='font-semibold text-[#5A7889]'>{message.objective}</span></p>
+          <hr className='w-full mt-[15px] border-[#5A7889]' />
+          <p className='mt-[15px] text-[18px] font-light'>Treść</p>
+          <p className='mt-[10px] h-[625px] overflow-y-auto pr-[20px]'>{message.message}</p>
+        </article>
+      </section>
+    </Suspense>
   )
 }
 
