ee1847b203b2f498ebca9181a9d1b26d6addd686
JcobV01 <jakubwadycki@gmail.com>
2024-11-04 09:32:17 +0100

    text-center - stopka md

--- components/footer/Navbar.jsx
+++ components/footer/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='flex gap-[25px] tracking-[2px] text-white text-[17px] font-semibold lg:text-[15px] md:flex-col md:gap-[10px]'>
+    <nav className='flex gap-[25px] tracking-[2px] text-white text-[17px] md:text-center font-semibold lg:text-[15px] md:flex-col md:gap-[10px]'>
         <Link href="/#parafia">PARAFIA</Link>
         <Link href="/#grupy">GRUPY PARAFIALNE</Link>
         <Link href="/#galeria">GALERIA</Link>
