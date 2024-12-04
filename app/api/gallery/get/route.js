38c99c73704d9d58c3c105f3c78b2bf678fe289d
ArtPleb2868 <plebanczyk.artur@gmail.com>
2024-12-04 12:15:38 +0100

    Dodanie funkcjonalności na stronie głównej galerii w management

--- app/api/gallery/get/route.js
+++ app/api/gallery/get/route.js
@@ -4,35 +4,63 @@ export const GET = async (req, res) => {
   if (req.method === 'GET') {
     try {
       // Pobieranie głównych folderów
-    const rootFolders = await cloudinary.api.root_folders();
+      const rootFolders = await cloudinary.api.root_folders();
 
-    const allResources = await cloudinary.api.resources({ type: 'upload' });
-    console.log(allResources.resources.map(r => r.public_id));
+      const allResources = await cloudinary.api.resources({ type: 'upload' });
+      console.log(allResources.resources.map(r => r.public_id));
 
-    // Pobranie zawartości każdego folderu
-    const foldersWithContent = await Promise.all(
-      rootFolders.folders.map(async (folder) => {
-        // Pobranie zasobów w folderze
-        const resources = await cloudinary.api.resources({
-          type: 'upload',
-          prefix: `${folder.path}/`,
+      // Pobranie zawartości każdego folderu
+      const foldersWithContent = await Promise.all(
+        rootFolders.folders.map(async (folder) => {
+          // Pobranie zasobów w folderze
+          const resources = await cloudinary.api.resources({
+            type: 'upload',
+            prefix: `${folder.path}/`,
+          });
+
+
+          return {
+            folderName: folder.name,
+            path: folder.path,
+            resources: resources.resources[0],
+          };
+        })
+      );
+
+      const groupByYear = (folders) => {
+        const grouped = {};
+
+        folders.forEach((folder) => {
+          const match = folder.folderName.match(/(\d{4})$/);
+          const year = match ? match[1] : "Inne";
+
+          if (!grouped[year]) grouped[year] = [];
+          grouped[year].push(folder);
         });
 
+        const sortedGrouped = Object.keys(grouped)
+          .sort((a, b) => {
+            if (a === "Inne") return 1;
+            if (b === "Inne") return -1;
 
-        return {
-          folderName: folder.name,
-          path: folder.path,
-          resources: resources.resources[0],
-        };
-      })
-    );
+            return parseInt(a) - parseInt(b);
+          })
+          .reduce((result, year) => {
+            result[year] = grouped[year];
+            return result;
+          }, {});
 
-    return new Response(JSON.stringify(foldersWithContent), { status: 200 });
-    } catch (error) {
-      console.error(error);
-      return new Response(JSON.stringify({ message: "Wystąpił błąd podczas wyszukiwania zdjęć." }), { status: 500 });
-    }
-  } else {
-    return new Response(JSON.stringify({ message: "Użyto niepoprawnej metody" }), { status: 405 });
+        return sortedGrouped;
+      };
+
+    const groupedFolders = groupByYear(foldersWithContent);
+
+    return new Response(JSON.stringify(groupedFolders), { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return new Response(JSON.stringify({ message: "Wystąpił błąd podczas wyszukiwania zdjęć." }), { status: 500 });
+  }
+} else {
+  return new Response(JSON.stringify({ message: "Użyto niepoprawnej metody" }), { status: 405 });
   }
 }
--- app/management/panel/galeria/page.jsx
+++ app/management/panel/galeria/page.jsx
@@ -11,6 +11,8 @@ const page = () => {
   const folderDialog = useRef(null)
   const [folders, setFolders] = useState([]);
   const [folderName, setFolderName] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
+  const [selectedYear, setSelectedYear] = useState("");
 
   const openDialogHandle = () => {
     folderDialog?.current?.showModal()
@@ -25,7 +27,7 @@ const page = () => {
   }
 
   const setNewFolder = async () => {
-    try{
+    try {
       const result = await fetch("/api/gallery/folder/create", {
         method: "POST",
         headers: {
@@ -34,21 +36,21 @@ const page = () => {
         body: JSON.stringify({ folderName: folderName })
       })
     }
-    catch(err){
+    catch (err) {
       console.error(err)
     }
   }
 
   useEffect(() => {
     const getData = async () => {
-      try{
+      try {
         const response = await fetch('/api/gallery/get')
         const data = await response.json()
         setFolders(data)
         console.log(data);
-        
+
       }
-      catch(err){
+      catch (err) {
         console.error("Błąd: " + err)
       }
     }
@@ -56,19 +58,61 @@ const page = () => {
     getData()
   }, [])
 
+  const filteredFolders = () => {
+    if (!searchQuery) return folders;
+
+    const lowerCaseQuery = searchQuery.toLowerCase();
+
+    // Filtrowanie folderów w każdym roku
+    const filtered = Object.entries(folders).reduce((acc, [year, folderList]) => {
+      const filteredList = folderList.filter(folder =>
+        folder.folderName.toLowerCase().includes(lowerCaseQuery)
+      );
+
+      if (filteredList.length > 0) {
+        acc[year] = filteredList; 
+      }
+
+      return acc;
+    }, {});
+
+    return filtered;
+  };
+
+  const getUniqueYears = () => {
+    const years = Object.keys(folders);
+    return [...years];
+  }
+
   return (
     <section className='w-full flex-1 pt-8'>
-      <button className='bg-[#11161A] py-[10px] px-[50px] rounded-[5px] text-white text-[20px] font-light w-full' onClick={openDialogHandle}>Dodaj album</button>
-      <div className='flex gap-3 w-full'>
-        {folders?.map((folder, index) => (
-          <div key={index} className='relative w-[200px] h-[230px] mt-[32px] cursor-pointer' data-name={folder?.folderName} onClick={(e) => moveToPhotos(e.currentTarget.dataset.name)}>
-            <Image src={folder?.resources?.secure_url} width={200} height={230} className='object-cover h-full brightness-50'/>
-            <p className='absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-[20px]'>{folder.folderName}</p>
+      <div className='w-full flex justify-between'>
+        <button className='bg-[#11161A] py-[10px] px-[50px] rounded-[5px] text-white text-[20px] font-light w-[300px]' onClick={openDialogHandle}>Dodaj album</button>
+        <select value={selectedYear} onChange={(e) => setSelectedYear(e.target.value)} className='w-[200px] px-[10px] rounded-[5px]'>
+          <option value="">Wybierz rok</option>
+          {getUniqueYears().map((year) => (
+            <option key={year} value={year}>{year}</option>
+          ))}
+        </select>
+        <input type="search" placeholder='Wyszukaj album' className='w-[600px] px-[10px] rounded-[5px]' value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
+      </div>
+      <div className='flex flex-col gap-3 w-full mt-8 h-[600px] px-[10px] overflow-y-auto'>
+        {Object.entries(filteredFolders()).map(([year, folders]) => (
+          <div className='w-full' key={year}>
+            <h2 className='text-[20px] text-center text-black'>{year}</h2>
+            <hr className='border-gray-400 w-full' />
+            <div className='flex flex-wrap gap-4'>
+              {folders.map((folder, index) => (
+                <div key={index} className='relative w-[200px] h-[230px] mt-[32px] cursor-pointer' data-name={folder?.folderName} onClick={(e) => moveToPhotos(e.currentTarget.dataset.name)}>
+                  <Image src={folder?.resources?.secure_url} width={200} height={230} className='object-cover h-full brightness-50' alt="Zdjęcie przedstawiające album" />
+                  <p className='absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-[20px] text-center'>{folder.folderName}</p>
+                </div>
+              ))}
+            </div>
           </div>
         ))}
       </div>
-
-      <NewFolderDialog dialogRef={folderDialog} closeDialog={closeDialogHandle} setFolderName={setFolderName} createFolder = {setNewFolder}/>
+      <NewFolderDialog dialogRef={folderDialog} closeDialog={closeDialogHandle} setFolderName={setFolderName} createFolder={setNewFolder} />
     </section>
   )
 }
