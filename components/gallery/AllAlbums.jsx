27266a1858edfdbddba70a0b3e250c0b7a9113c9
ArtPleb2868 <plebanczyk.artur@gmail.com>
2024-12-17 09:11:50 +0100

    Grid do galerii

--- components/gallery/AllAlbums.jsx
+++ components/gallery/AllAlbums.jsx
@@ -31,17 +31,27 @@ const AllAlbums = () => {
         getAlbums();
     }, []);
 
-    console.log(albums);
-
     return (
         <article className='mt-[90px] grid grid-cols-4 gap-[20px]'>
             {albums.map((album, index) => {
+                const dynamicAlbumsWidthEverySeven = (index + 1) % 7 === 0;
+                const dynamicAlbumsWIdthEveryFiveteen = (index + 1) % 15 === 0;
+
+                let imageWidth = 250;
+                let imageHeight = 250;
+                if (dynamicAlbumsWidthEverySeven){
+                    imageWidth = 520;
+                } else if (dynamicAlbumsWIdthEveryFiveteen){
+                    imageWidth = 520;
+                    imageHeight = 520;
+                }
+
                 return (
-                    <div key={album.folderId} className={`flex-center h-[250px] w-[250px] rounded-[2px] relative`}>
+                    <div key={album.folderId} className={`flex-center rounded-[2px] relative ${dynamicAlbumsWidthEverySeven ? 'col-span-2 w-[520px] h-[250px]' : 'w-[250px] h-[250px]'} ${dynamicAlbumsWIdthEveryFiveteen ? 'col-span-2 col-col-2 w-[520px] h-[520px]' : 'w-[250px] h-[250px]'}`}>
                         <hr className='absolute top-[15px] w-[100px] h-[1px] bg-white z-10' />
                         <p className='text-white text-[25px] font-light max-w-[200px] text-center z-10'>{album.name}</p>
                         <hr className='absolute bottom-[15px] w-[100px] h-[1px] bg-white z-10' />
-                        <Image src={album.cover} width="250" height="250" className='absolute w-full h-full brightness-50' />
+                        <Image src={album.cover} width={imageWidth} height={imageHeight} className='absolute w-full h-full brightness-50 object-cover' />
                     </div>
                 )
             })}
