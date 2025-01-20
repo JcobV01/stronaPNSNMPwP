4596ff536d0d0fd790bba1ed51ac84b1e0efe0be
ArtPleb2868 <plebanczyk.artur@gmail.com>
2025-01-20 09:22:48 +0100

    Dodanie podpisu do stopki

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,7 +5,9 @@ import Logo from '@components/Logo'
 import Navbar from '@components/footer/Navbar'
 import IconFooter from '@components/footer/IconFooter'
 import DownloadFile from '@components/DownloadFile'
-
+import Link from 'next/link'
+import Image from 'next/image'
+import logoAirtilion from '@public/assets/images/airtilionFooter.svg'
 
 const Footer = () => {
   const year = new Date().getFullYear();
@@ -26,9 +28,11 @@ const Footer = () => {
           </div>
           <DownloadFile location='/assets/downloadFiles/STANDARDY-OCHRONY-DZIECI.pdf' text='Standardy Ochrony Dzieci' />
           <p className='text-[13px] tracking-[3px] text-white text-center w-[50%] 2xl:w-[60%] lg:w-[80%] lg:text-[11px]'>Informujemy, że wszystkie dane osobowe zamieszczone na stronie internetowej parafii są umieszczone wyłącznie za zgodą osób zainteresowanych.</p>
-          <div className='flex justify-between w-full lg:flex-col lg:gap-[10px]'>
-            <p className='text-[#898989] text-[10px] tracking-[2.6px]'>{`©${year} Parafia pw. Niepokalanego Serca Najświętszej Maryi Panny`}</p>
-            <p className='text-[#898989] text-[10px] tracking-[2.6px]'>Made by Artur Plebańczyk & Jakub Wadycki</p>
+          <div className='flex items-center justify-between w-full lg:flex-col lg:gap-[10px]'>
+            <p className='text-[#898989] text-[10px] tracking-[2.6px] sm:text-center'>{`©${year} Parafia pw. Niepokalanego Serca Najświętszej Maryi Panny`}</p>
+            <Link href="https://airtilion.com" title="Przejdź na stronę Airtilion" className='text-[#9d9d9d] text-[12px] font-light flex items-center gap-[10px] duration-700 mt-[12px] hover:scale-110 sm:flex-col'>Projekt i wykonanie
+              <Image src={logoAirtilion} alt="Logo Airtilion" width="138" height="15"/>
+            </Link>
           </div>
         </footer>
       }
