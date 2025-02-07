fb2992d4e370a9340fcdef48df661ea230018967
ArtPleb2868 <plebanczyk.artur@gmail.com>
2025-02-07 13:42:10 +0100

    Dodanie wyłączania menu po kliknięciu w link

--- components/header/Navbar.jsx
+++ components/header/Navbar.jsx
@@ -24,6 +24,8 @@ const Navbar = () => {
     };
   }, []);
 
+  const closeMenu = () => setActive(false);
+
   return (
     <>
       <nav className={`${scrolled === true ? 'backdrop-blur-[5px] bg-[rgba(0,0,0,0.30)]' : ''} md:hidden flex justify-center sticky top-0 duration-700 z-[1000] gap-[30px] items-center h-[75px] tracking-[2px] text-white text-[17px] lg:text-[15px] md:flex-col md:gap-[10px]`}>
@@ -42,12 +44,12 @@ const Navbar = () => {
 
         <div className={active ? "hidden md:flex justify-center gap-4 absolute top-0 left-0 w-full h-dvh bg-[rgba(0,0,0,0.90)] z-[110] transition-all duration-500 ease-in-out transform translate-x-0" : "fixed md:hidden w-full h-screen bg-white transition-all duration-500 ease-in-out transform translate-x-full"}>
           <nav className='flex-center flex-col h-full text-white gap-5'>
-            <Link href="/#parafia">Parafia</Link>
-            <Link href="/#grupy">Grupy parafialne</Link>
-            <Link href="/#galeria">Galeria</Link>
-            <Link href="/#kontakt">Kontakt</Link>
-            <Link href="/komunikaty">Komunikaty</Link>
-            <Link href="/ogloszenia">Ogłoszenia</Link>
+            <Link href="/#parafia" onClick={closeMenu}>Parafia</Link>
+            <Link href="/#grupy" onClick={closeMenu}>Grupy parafialne</Link>
+            <Link href="/#galeria" onClick={closeMenu}>Galeria</Link>
+            <Link href="/#kontakt" onClick={closeMenu}>Kontakt</Link>
+            <Link href="/komunikaty" onClick={closeMenu}>Komunikaty</Link>
+            <Link href="/ogloszenia" onClick={closeMenu}>Ogłoszenia</Link>
           </nav>
         </div>
 
