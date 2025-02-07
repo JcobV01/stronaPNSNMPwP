e2815b279d904dbb9858f8b4af352b7d6b01ad6b
ArtPleb2868 <plebanczyk.artur@gmail.com>
2025-02-07 10:44:26 +0100

    Optymalizacja ikon w headerze (zmiana koloru i zabezpieczenie _target)

--- components/header/DynamicHeader.jsx
+++ components/header/DynamicHeader.jsx
@@ -9,6 +9,7 @@ import Logo from '@components/Logo'
 import IconHeader from '@components/header/IconHeader'
 import Navbar from '@components/header/Navbar';
 
+
 import mainImage from '@public/assets/images/header-images/main-image.webp';
 import winterImage from '@public/assets/images/header-images/winter-main.webp';
 import cemetery from '@public/assets/images/header-images/cemetery.webp';
--- components/header/IconHeader.jsx
+++ components/header/IconHeader.jsx
@@ -5,8 +5,8 @@ import React from 'react'
 const IconHeader = ({imgSource, destination, desc}) => {
   return (
     <div className='bg-none'>
-        <Link href={destination} target='_blank' aria-label={desc} title={`Przejdź do ${desc}`} className='inline-flex duration-700 hover:scale-95'>
-            <Icon icon={imgSource} width="50" height="50" alt={`Ikona ${desc}`} className='text-[#5A7889] sm:w-[40px] sm:h-[40px] duration-700 hover:text-[#ECECEC] hover:scale-95'/>
+        <Link href={destination} target='_blank' rel="noopener noreferrer" aria-label={desc} title={`Przejdź do ${desc}`} className='inline-flex duration-700 hover:scale-95'>
+            <Icon icon={imgSource} width="50" height="50" alt={`Ikona ${desc}`} className='text-[#ECECEC] sm:w-[40px] sm:h-[40px] duration-700 hover:text-[#5A7889] hover:scale-95'/>
         </Link>
     </div>
   )
