45f97d3707531baffa3d2a55a4c4549ee549f010
ArtPleb2868 <plebanczyk.artur@gmail.com>
2025-02-11 19:02:09 +0100

    Powrót do starych opcji

--- next.config.mjs
+++ next.config.mjs
@@ -6,19 +6,10 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'przybyslawice.diecezja.tarnow.pl',
-        pathname: '/uploads/**',
+        port: '4242',
+        pathname: '/uploads/**'
       },
-      // Dodaj drugi wzorzec dla wersji bez portu
-      {
-        protocol: 'https',
-        hostname: 'przybyslawice.diecezja.tarnow.pl',
-        pathname: '**', // Zezwól na dowolne ścieżki
-      }
     ],
-    // Dodaj te nowe opcje z Next.js 14
-    formats: ['image/avif', 'image/webp'],
-    minimumCacheTTL: 3600,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
 };
 
