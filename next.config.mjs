364b7adacb26905d17a231dd80e452266b75e542
ArtPleb2868 <plebanczyk.artur@gmail.com>
2025-02-11 18:56:02 +0100

    testy cd

--- next.config.mjs
+++ next.config.mjs
@@ -6,9 +6,24 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'przybyslawice.diecezja.tarnow.pl',
-        pathname: '/uploads/**'
+        pathname: '/uploads/**',
       },
+      // Dodaj drugi wzorzec dla wersji bez portu
+      {
+        protocol: 'https',
+        hostname: 'przybyslawice.diecezja.tarnow.pl',
+        pathname: '**', // Zezwól na dowolne ścieżki
+      }
     ],
+    // Dodaj te nowe opcje z Next.js 14
+    formats: ['image/avif', 'image/webp'],
+    minimumCacheTTL: 3600,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+  // Nowa opcja eksperymentalna dla Next.js 14
+  experimental: {
+    optimizeCss: true,
+    missingSuspenseWithCSRBailout: false,
   },
 };
 
