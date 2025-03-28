e79fd13d54d33c0bd04347b470ef9013b70121bc
ArtPleb2868 <plebanczyk.artur@gmail.com>
2025-03-28 12:33:20 +0100

    Dodanie sitemap i robots.txt

---
+++ public/sitemap.js
@@ -0,0 +1,100 @@
+export default function sitemap() {
+    return [
+      {
+        url: 'https://przybyslawice.diecezja.tarnow.pl',
+        lastModified: new Date(),
+        changeFrequency: 'yearly',
+        priority: 1,
+      },
+      {
+        url: 'https://przybyslawice.diecezja.tarnow.pl/ogloszenia',
+        lastModified: new Date(),
+        changeFrequency: 'yearly',
+        priority: 0.9,
+      },
+      {
+        url: 'https://przybyslawice.diecezja.tarnow.pl/galeria',
+        lastModified: new Date(),
+        changeFrequency: 'yearly',
+        priority: 0.9,
+      },
+      {
+        url: 'https://przybyslawice.diecezja.tarnow.pl/historia',
+        lastModified: new Date(),
+        changeFrequency: 'yearly',
+        priority: 0.7,
+      },
+      {
+        url: 'https://przybyslawice.diecezja.tarnow.pl/nabozenstwa',
+        lastModified: new Date(),
+        changeFrequency: 'yearly',
+        priority: 0.7,
+      },
+      {
+        url: 'https://przybyslawice.diecezja.tarnow.pl/rodacy',
+        lastModified: new Date(),
+        changeFrequency: 'yearly',
+        priority: 0.7,
+      },
+      {
+        url: 'https://przybyslawice.diecezja.tarnow.pl/duszpasterze',
+        lastModified: new Date(),
+        changeFrequency: 'yearly',
+        priority: 0.7,
+      },
+      {
+        url: 'https://przybyslawice.diecezja.tarnow.pl/cmentarz',
+        lastModified: new Date(),
+        changeFrequency: 'yearly',
+        priority: 0.7,
+      },
+      {
+        url: 'https://przybyslawice.diecezja.tarnow.pl/sakramenty',
+        lastModified: new Date(),
+        changeFrequency: 'yearly',
+        priority: 0.7,
+      },
+      {
+        url: 'https://przybyslawice.diecezja.tarnow.pl/komunikaty',
+        lastModified: new Date(),
+        changeFrequency: 'yearly',
+        priority: 0.7,
+      },
+      {
+        url: 'https://przybyslawice.diecezja.tarnow.pl/grupy-parafialne/lso',
+        lastModified: new Date(),
+        changeFrequency: 'yearly',
+        priority: 0.5,
+      },
+      {
+        url: 'https://przybyslawice.diecezja.tarnow.pl/grupy-parafialne/schola',
+        lastModified: new Date(),
+        changeFrequency: 'yearly',
+        priority: 0.5,
+      },
+      {
+        url: 'https://przybyslawice.diecezja.tarnow.pl/grupy-parafialne/ak',
+        lastModified: new Date(),
+        changeFrequency: 'yearly',
+        priority: 0.5,
+      },
+      {
+        url: 'https://przybyslawice.diecezja.tarnow.pl/grupy-parafialne/dsm',
+        lastModified: new Date(),
+        changeFrequency: 'yearly',
+        priority: 0.5,
+      },
+      {
+        url: 'https://przybyslawice.diecezja.tarnow.pl/grupy-parafialne/roze',
+        lastModified: new Date(),
+        changeFrequency: 'yearly',
+        priority: 0.5,
+      },
+      {
+        url: 'https://przybyslawice.diecezja.tarnow.pl/grupy-parafialne/rada',
+        lastModified: new Date(),
+        changeFrequency: 'yearly',
+        priority: 0.5,
+      },
+    ]
+  }
